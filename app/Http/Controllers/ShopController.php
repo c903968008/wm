244c21317ffa448d2ae553d65628eb77aa7a6f54
cai <903968008@qq.com>
2019-03-05 18:18:46 +0800

    test

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -17,6 +17,12 @@ class ShopController extends Controller
     //展示所有商铺信息
     public function showAll()
     {
+        $shop = $this->shopRespository->qureyAll();
+        if($shop){
+            return $this->success($shop,"获取所有商铺信息成功");
+        } else{
+            return $this->fail($shop,'获取所有商铺信息失败');
+        }
     
     }
 
